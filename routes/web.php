b68620395a66a72e2042a2c1af988ab64687bdeb
Iris Claire Solano <iris.solano@sorsu.edu.ph>
2025-05-22 13:25:03 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,26 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        "message" => "Welcome to the Library Management API",
+        "endpoints" => [
+            "books" => [
+                "GET /books" => "List all books",
+                "POST /books" => "Create a new book"
+            ],
+            "users" => [
+                "GET /users" => "List all users",
+                "POST /login" => "User login",
+                "GET /user" => "Get current user info (authenticated)"
+            ],
+            "transactions" => [
+                "GET /transactions" => "List all transactions",
+                "GET /transactions/{id}" => "Get details of a specific transaction"
+            ],
+            "borrow_return" => [
+                "POST /borrow" => "Borrow a book",
+                "POST /return/{id}" => "Return a borrowed book by transaction id"
+            ]
+        ]
+    ]);                        
 });
